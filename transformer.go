308b22c2ee7c59c5f4f0db70f35591e30ea3fe7f
Matt Fischer <matt@mattfischer.com>
2018-05-04 19:02:45 -0600

    Support for maps
    This change adds support for maps which includes the sub-types
    of certificates and rsa keys. It is not general purpose map
    support. Since this change will also add a "guess" of the data
    type rather than just stating "value" a note about double-checking
    the output is added.
    
    The code has a bit of a hack. Since the format of the input
    can vary widely we use a poor-man's union by allowing that an Importable
    can contain either a value directly as a string or a map. The YAML
    library will now allow us to use the name 'value' for the same field,
    even though only one would appear at a time. This means that we have to
    set a different element name for maps - and then change the name before
    printing the results. This is a bit hacky but is significantly simpler
    than hand-writing our own marshal/unmarshal code which would then have
    to be maintained and tested.

--- transformer.go
+++ transformer.go
@@ -1,51 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"io"
+    "fmt"
+    "io"
+    "strings"
 
-	"gopkg.in/yaml.v2"
+    "gopkg.in/yaml.v2"
 )
 
 // Importable represents a credential to be loaded into Credhub via `bulk-import`
 type Importable struct {
-	Name  string `yaml:"name"`
-	Type  string `yaml:"type"`
-	Value string `yaml:"value"`
+    Name   string `yaml:"name"`
+    Type   string `yaml:"type"`
+    Value  string `yaml:"value,omitempty"`
+    SubMap map[interface{}]interface{} `yaml:"subMapValue,omitempty"`
 }
 
 // BulkImport represents what will be actually sent to Credhub
 type BulkImport struct {
-	Credentials []Importable `yaml:"credentials"`
+    Credentials []Importable `yaml:"credentials"`
+}
+
+// Utility to test if something is a map
+func isMap(x interface{}) bool {
+    t := fmt.Sprintf("%T", x)
+    return strings.HasPrefix(t, "map[")
+}
+
+func handleMap(mapVal map[interface{}]interface{}, prefix string, parentKey string) Importable {
+    return Importable{
+        Name: fmt.Sprintf("%s/%s", prefix, parentKey),
+        Type: getType(parentKey, fmt.Sprint(mapVal)),
+        SubMap: mapVal,
+    }
+}
+
+func getType(key string, valStr string) string {
+    //attempt to guess the type for the item, default to "value"
+    if strings.Contains(key, "password") || strings.Contains(key, "secret") {
+        return "password"
+    } else if strings.Contains(key, "user") || strings.Contains(key, "login") {
+        return "user"
+    //the cert check should be above rsa since a cert may also contain a pk
+    } else if strings.Contains(valStr, "CERTIFICATE") {
+        return "certificate"
+    } else if strings.Contains(valStr, "KEY---") {
+        return "rsa"
+    }
+    return "value"
+}
+
+func makeImportable(prefix string, key string, valStr string) Importable {
+    return Importable{
+        Name:  fmt.Sprintf("%s/%s", prefix, key),
+        Type:  getType(key, valStr),
+        Value: valStr,
+    }
 }
 
 // Transform performs the translation from vars file to import file
 func Transform(prefix string, input io.Reader) (BulkImport, error) {
-	decoder := yaml.NewDecoder(input)
+    decoder := yaml.NewDecoder(input)
 
-	var pipelineVars map[interface{}]interface{}
-	decoder.Decode(&pipelineVars)
+    var pipelineVars map[interface{}]interface{}
+    decoder.Decode(&pipelineVars)
 
-	vals := make([]Importable, 0, len(pipelineVars))
-	for key, val := range pipelineVars {
+    vals := make([]Importable, 0, len(pipelineVars))
+    for key, val := range pipelineVars {
 
-		// Let's require, for now, simple types only in the var field
-		var valStr string
-		switch v := val.(type) {
-		default:
-			return BulkImport{}, fmt.Errorf("Invalid value type in vars file %T. Currently only primitive values are supported", v)
-		case bool, float32, float64, int, int16, int32, int64, string, uint, uint16, uint32, uint64:
-			valStr = fmt.Sprint(val)
-		}
+        // Let's require, for now, simple types only in the var field
+        var valStr string
+        switch v := val.(type) {
+        default:
+            if isMap(val) {
+                vals = append(vals, handleMap(val.(map[interface{}]interface{}), prefix, key.(string)))
+            } else {
+                return BulkImport{}, fmt.Errorf("Invalid value type in vars file %T. Currently only primitive values are supported", v)
+            }
+        case bool, float32, float64, int, int16, int32, int64, string, uint, uint16, uint32, uint64:
+            valStr = fmt.Sprint(val)
+            vals = append(vals, makeImportable(prefix, key.(string), valStr))
+        }
+    }
 
-		vals = append(vals, Importable{
-			Name:  fmt.Sprintf("%s/%s", prefix, key),
-			Type:  "value",
-			Value: valStr,
-		})
-	}
-
-	return BulkImport{
-		Credentials: vals,
-	}, nil
+    return BulkImport{
+        Credentials: vals,
+    }, nil
 }
